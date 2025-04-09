c20246c2ed8e3168b96b82c2d5990cad67992a0f
Rizvan337 <rizvanichu337@gmail.com>
2025-04-09 14:39:07 +0530

    User deletion done

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -3,18 +3,19 @@ import User from '../models/User';
 import { verifyToken } from '../middlewares/authMiddleware';
 import express, { Request, Response } from 'express';
 const router = express.Router();
+import { asyncHandler } from '../utils/asynHandler';
 
 // Get all users
-router.get('/users', verifyToken, async (req, res) => {
+router.get('/users', verifyToken, asyncHandler(async(req, res) => {
   try {
     const users = await User.find({}, '-password'); // exclude password
     res.json(users);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch users' });
-  }
-});
-
-router.put('/users/:id', verifyToken, async (req: Request, res: Response): Promise<void> => {
+  } 
+})
+);
+router.put('/users/:id', verifyToken, asyncHandler(async (req: Request, res: Response): Promise<void> => {
   try {
     const { name, email } = req.body;
 
@@ -35,6 +36,26 @@ router.put('/users/:id', verifyToken, async (req: Request, res: Response): Promi
     console.error('Update error:', error);
     res.status(500).json({ message: 'Server error' });
   }
-});
+})
+);
+
+
+// DELETE user by ID
+router.delete('/users/:id', verifyToken,asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'User deleted successfully', id: req.params.id });
+  } catch (error) {
+    console.error('Delete error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+})
+);
+
 
 export default router;
